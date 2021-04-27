cc0cea1c1c1d000bfb5d6b65265d6156f7cd7838
nyingi <nyingi.maina@gmail.com>
2021-04-27 20:00:09 +0300

    Setup project

---
+++ Rocket.Libraries.Delta.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Rocket.Libraries.Delta.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
