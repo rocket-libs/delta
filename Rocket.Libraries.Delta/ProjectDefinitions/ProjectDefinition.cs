e0b8efbbdfff2921bd83b7777daeb35b08fd2163
nyingi <nyingimaina@gmail.com>
2021-07-23 08:06:24 +0300

    Functionality to get project labels

--- Rocket.Libraries.Delta/ProjectDefinitions/ProjectDefinition.cs
+++ Rocket.Libraries.Delta/ProjectDefinitions/ProjectDefinition.cs
@@ -4,6 +4,8 @@ namespace Rocket.Libraries.Delta.ProjectDefinitions
 {
     public class ProjectDefinition
     {
+        public string Label { get; set; }
+
         public Guid ProjectId { get; set; }
 
         public string ProjectPath { get; set; }
--- Rocket.Libraries.Delta/ProjectDefinitions/ProjectDefinitionsReader.cs
+++ Rocket.Libraries.Delta/ProjectDefinitions/ProjectDefinitionsReader.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Rocket.Libraries.Delta.FileSystem;
+using Rocket.Libraries.Delta.Projects;
 
 namespace Rocket.Libraries.Delta.ProjectDefinitions
 {
     public interface IProjectDefinitionsReader
     {
         Task<ImmutableList<ProjectDefinition>> GetAllProjectDefinitionsAsync();
+
         Task<ProjectDefinition> GetSingleProjectDefinitionByIdAsync(Guid projectId);
     }
 
@@ -17,30 +19,45 @@ namespace Rocket.Libraries.Delta.ProjectDefinitions
     {
         private readonly IFileSystemAccessor fileReader;
 
-        public ProjectDefinitionsReader (
-            IFileSystemAccessor fileReader)
+        private readonly IProjectReader projectReader;
+
+        public ProjectDefinitionsReader(
+            IFileSystemAccessor fileReader,
+            IProjectReader projectReader)
         {
             this.fileReader = fileReader;
+            this.projectReader = projectReader;
         }
 
-        public async Task<ImmutableList<ProjectDefinition>> GetAllProjectDefinitionsAsync ()
+        public async Task<ImmutableList<ProjectDefinition>> GetAllProjectDefinitionsAsync()
         {
             try
             {
-                await Semaphore.WaitAsync ();
-                var serializedProjects = await fileReader.GetAllTextAsync (ProjectsDefinitionStoreFile);
-                return JsonSerializer.Deserialize<ImmutableList<ProjectDefinition>> (serializedProjects);
+                await Semaphore.WaitAsync();
+                var serializedProjects = await fileReader.GetAllTextAsync(ProjectsDefinitionStoreFile);
+                var projectDefinitions = JsonSerializer.Deserialize<ImmutableList<ProjectDefinition>>(serializedProjects);
+                InjectDisplayLabels(projectDefinitions);
+                return projectDefinitions;
             }
             finally
             {
-                Semaphore.Release ();
+                Semaphore.Release();
             }
         }
 
-        public async Task<ProjectDefinition> GetSingleProjectDefinitionByIdAsync (Guid projectId)
+        public async Task<ProjectDefinition> GetSingleProjectDefinitionByIdAsync(Guid projectId)
         {
-            var allProjectDefinitions = await GetAllProjectDefinitionsAsync ();
-            return allProjectDefinitions.SingleOrDefault (project => project.ProjectId == projectId);
+            var allProjectDefinitions = await GetAllProjectDefinitionsAsync();
+            return allProjectDefinitions.SingleOrDefault(project => project.ProjectId == projectId);
+        }
+
+        private void InjectDisplayLabels(ImmutableList<ProjectDefinition> projectDefinitions)
+        {
+            foreach (var specificProjectDefinition in projectDefinitions)
+            {
+                var project = projectReader.GetByPath(specificProjectDefinition.ProjectPath);
+                specificProjectDefinition.Label = project.Label;
+            }
         }
     }
 }
