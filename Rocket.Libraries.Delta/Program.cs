f972ce63f82bf6d6a464f9053a41c570427eacd1
nyingi <nyingi.maina@gmail.com>
2021-12-12 11:56:57 +0300

    Added functionality to allow option loading of web ui

--- Rocket.Libraries.Delta/Program.cs
+++ Rocket.Libraries.Delta/Program.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -10,43 +13,66 @@ namespace delta
 {
     public class Program
     {
-        public static IHostBuilder CreateHostBuilder(string[] args)
+        const int port = 5002;
+        public static IHostBuilder CreateHostBuilder (string[] args)
         {
 
-            
-            return Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder =>
-                    {
-                        webBuilder.UseStartup<Startup>()
-                        .ConfigureLogging((hostingContext, logging) =>
+            return Host.CreateDefaultBuilder (args)
+                .ConfigureWebHostDefaults (webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup> ()
+                        .ConfigureLogging ((hostingContext, logging) =>
                         {
-                            logging.ClearProviders();
-                            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                            logging.SetMinimumLevel(LogLevel.Trace);
-                            logging.AddConsole();
-                            logging.AddDebug();
-                            logging.AddEventSourceLogger();
+                            logging.ClearProviders ();
+                            logging.AddConfiguration (hostingContext.Configuration.GetSection ("Logging"));
+                            logging.SetMinimumLevel (LogLevel.Trace);
+                            logging.AddConsole ();
+                            logging.AddDebug ();
+                            logging.AddEventSourceLogger ();
                         })
-                        .UseNLog()
-                        .UseContentRoot(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
-                        .UseUrls($"http://0.0.0.0:5002");
-                    });
+                        .UseNLog ()
+                        .UseContentRoot (Path.GetDirectoryName (Assembly.GetEntryAssembly ().Location))
+                        .UseUrls ($"http://0.0.0.0:{port}");
+                });
         }
 
-        public static void Main(string[] args)
+        public static void Main (string[] args)
         {
-            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            var logger = NLogBuilder.ConfigureNLog ("nlog.config").GetCurrentClassLogger ();
             try
             {
-                CreateHostBuilder(args).Build().Run();
+                LaunchWebUIIfRequested (args);
+                CreateHostBuilder (args).Build ().Run ();
             }
             catch (System.Exception e)
             {
-                logger.Error(e, "Unhandled exception caught in the global try-catch block... This is bad");
+                logger.Error (e, "Unhandled exception caught in the global try-catch block... This is bad");
             }
             finally
             {
-                NLog.LogManager.Shutdown();
+                NLog.LogManager.Shutdown ();
+            }
+        }
+
+        private static bool HasArg (string[] args, string arg)
+        {
+            if (args == null || args.Length == 0)
+            {
+                return false;
+            }
+            return args.Any (a => a.Equals (arg, StringComparison.InvariantCulture));
+        }
+
+        private static void LaunchWebUIIfRequested (string[] args)
+        {
+            if (HasArg (args, "--webui"))
+            {
+
+                Process.Start (new ProcessStartInfo
+                {
+                    FileName = $"http://localhost:{port}/index.html",
+                        UseShellExecute = true
+                });
             }
         }
     }
